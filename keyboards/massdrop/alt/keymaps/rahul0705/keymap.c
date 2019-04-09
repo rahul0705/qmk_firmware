3a502833619dc7df5542b8dd4816316d3aa5e5ff
Rahul Mohandas <rahul0705@gmail.com>
2019-04-09 19:54:01 +0000

    feat(led): each layer has its own LED Mode

--- keyboards/massdrop/alt/keymaps/rahul0705/keymap.c
+++ keyboards/massdrop/alt/keymaps/rahul0705/keymap.c
@@ -1,9 +1,9 @@
 #include QMK_KEYBOARD_H
 
 enum alt_layers {
-    //_LOCKED,
-    _BASE,
-    _FUNCTION,
+    BASE,
+    FUNC,
+    EMJI,
 };
 
 enum alt_keycodes {
@@ -15,6 +15,15 @@ enum alt_keycodes {
     DBG_MOU,               //DEBUG Toggle Mouse Prints
     MD_BOOT,               //Restart into bootloader after hold timeout
     KC_RHL,                //Rahul
+    SHRUG,                 //Shrug
+};
+
+enum unicode_names {
+  POOP,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+  [POOP] = 0x1F4A9,
 };
 
 #define TG_NKRO MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
@@ -32,21 +41,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------+--------------------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   Ctrl(B)   |  Up  | Pgdn |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Meta |  Alt |                      Space                     |  Alt | MO(0)| Left | Down | Right|
+ * | Ctrl | Meta |  Alt |                      Space                     |  Alt | FUNC | Left | Down | Right|
  * `--------------------------------------------------------------------------------------------------------'
  */
-    [_BASE] = LAYOUT(
+    [BASE] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LCTL(KC_B),       KC_UP,   KC_PGDN, \
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(FUNC),KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 /* Function Layer
  * ,--------------------------------------------------------------------------------------------------------.
  * | Rahul|  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Bksp | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | L_SPI| L_BRI| L_SPD| L_HUI| L_SAI|      |   ?  |   ?  |      | Print| Scrlk| Pause|      | End  |
+ * |      | L_SPI| L_BRI| L_SPD| L_HUI| L_SAI|      |   ?  |   ?  |      | Print| Scrlk| Pause| EMJI | End  |
  * |------+------+------+------+------+-------------+------+------+------+------+------+------+------+------|
  * |      |L_MODP| L_DIM|L_MODN| L_HUD| L_SAD|      |      |      |      |      |      |             | VolU |
  * |------+------+------+------+------+------|------+------+------+------+------+------+--------------------|
@@ -55,14 +64,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |                                                |      |      | Home | Pgdn | End  |
  * `--------------------------------------------------------------------------------------------------------'
  */
-    [_FUNCTION] = LAYOUT(
+    [FUNC] = LAYOUT(
         KC_RHL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_MUTE, \
-        KC_TRNS, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, KC_TRNS, U_T_AUTO,U_T_AGCR,KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_END, \
+        KC_TRNS, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, KC_TRNS, U_T_AUTO,U_T_AGCR,KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, TO(EMJI),KC_END,  \
         KC_TRNS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_VOLU, \
         KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, MD_BOOT, TG_NKRO, DBG_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_PGUP, KC_VOLD, \
-        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END  \
+        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END   \
     ),
-/* Function Layer
+/* Emoji Layer
+ * ,--------------------------------------------------------------------------------------------------------.
+ * | BASE |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------+------+------+------|
+ * |      |      | POOP |      |      |      |      |      |      |      |      |      |             |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------+--------------------|
+ * |      |      |      |      |      |      |      |      |      |      | SHRUG|             |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |                                                |      |      |      |      |      |
+ * `--------------------------------------------------------------------------------------------------------'
+ */
+    [EMJI] = LAYOUT(
+        TO(BASE),KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, X(POOP), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SHRUG,   KC_TRNS,          KC_TRNS, KC_TRNS, \
+        KC_TRNS, UC_M_WC, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+    ),
+
+/* Layer
  * ,--------------------------------------------------------------------------------------------------------.
  * |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |   K  |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -88,6 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    rgb_matrix_mode(RGB_MATRIX_CYCLE_UP_DOWN);
 };
 
 // Runs constantly in the background, in a loop.
@@ -97,6 +128,21 @@ void matrix_scan_user(void) {
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
 #define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
+
+uint32_t layer_state_set_user(uint32_t state){
+    switch (biton32(state)){
+        case FUNC:
+            rgb_matrix_mode(RGB_MATRIX_RAINDROPS);
+            break;
+        case EMJI:
+            rgb_matrix_mode(RGB_MATRIX_RAINBOW_PINWHEELS);
+            break;
+        default:
+            rgb_matrix_mode(RGB_MATRIX_CYCLE_UP_DOWN);
+            break;
+    }
+    return state;
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
@@ -144,6 +190,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_RHL:
             if (record->event.pressed) {
                 SEND_STRING("Rahul Mohandas");
+            }
+            return false;
+        case SHRUG:
+            if (record->event.pressed) {
+                send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
             }
             return false;
         default:
